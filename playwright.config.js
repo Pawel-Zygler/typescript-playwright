cb7f3c0587716b5afa4008f3ab7c436abc7748b6
zygler.pawel@protonmail.com <zygler.pawel@protonmail.com>
2024-01-06 22:59:59 +0100

    Headless removed from config.

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,6 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: true,
     trace: "on-first-retry",
   },
 
